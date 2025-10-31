4a355b12371c0621afd8ff2ae8e25fcab4b85834
Diego Martínez Varela <a01663988@tec.mx>
2025-10-31 13:28:41 -0600

    Actualizamos la parte del separador para hacerlo funcional y añadimos el entorno virtual

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,13 +1,12 @@
 import pandas as pd
 
-dataFile = pd.read_csv('figuras.csv')
+dataFile = pd.read_csv('figuras.csv', sep = '\t')
 
-print("Procesando figuras.../n")
-
+print("Procesando figuras...\n")
 
 areas = []
 perimetros = []
 
-for index,row in dataFile.iterrows():
-	print(f"Fila {index}: Figura = {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['Medida2']}")
+for index, row in dataFile.iterrows():
+    print(f"Fila {index}: Figura = {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['MEDIDA2']}")
 
