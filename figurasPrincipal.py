c3a9c0f313eddf345f441ed55e5ab8be47f97564
Diego Martínez Varela <a01663988@tec.mx>
2025-10-31 14:11:24 -0600

    Añadimos comentarios al código

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,18 +1,22 @@
 import pandas as pd
-from areas import area
-dataFile = pd.read_csv('figuras.csv', sep = '\t')
+from areas import area #Del archivo areas importamos la funcion area
 
-print("Procesando figuras...\n")
+dataFile = pd.read_csv('figuras.csv', sep = '\t') #Se modificó el codigo para que leeyera el datafile separado por espacios
 
-areas = []
-perimetros = []
+print("Procesando figuras...\n") # Mensaje de encabezado para indicar lo que se está haciendo
 
-for index, row in dataFile.iterrows():
-	figura = row['FIGURA']
-	m1 = row['MEDIDA1']
-	m2 = row['MEDIDA2']
+areas = [] # Se inicializa una lista vacia de nombre 'areas'
+perimetros = [] # Se inicializa una lista vacia de nombre 'perimetros'
+
+for index, row in dataFile.iterrows(): # Se inicializa un ciclo for que itera sobre cada fila de cada índice del datafile
+	figura = row['FIGURA'] # Se le asigna el valor que esté en la fila de Figura a 'figura'
+	m1 = row['MEDIDA1'] # Se le asigna el valor que esté en la fila de MEDIDA1 a 'm1'
+	m2 = row['MEDIDA2'] # Se le asigna el valor que esté en la fila de MEDIDA1 a 'm2'
 	a = area(figura, m1, m2)
-	areas.append(a)
+        # Lo que retorne la función 'area' al pasarle los parámetros figura, m1, m2 se guardará en la variable 'a'
+	areas.append(a) # Se agrega el valor de 'a' a la lista 'areas'
 	print(f"Fila {index}: Figura = {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['MEDIDA2']}, Area = {a}")
+        # Se imprime como f-string la figura que es, sus medidas y su área 
+
 
 
