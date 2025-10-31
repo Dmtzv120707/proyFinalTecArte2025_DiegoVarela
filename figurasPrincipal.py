4b8ba09c687d7f7b370f229ec3dac51dde8ef6a0
Diego Martínez Varela <a01663988@tec.mx>
2025-10-31 13:59:00 -0600

    Añadimos funcion de áreas en un archivo nuevo y se complemento

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from areas import area
 dataFile = pd.read_csv('figuras.csv', sep = '\t')
 
 print("Procesando figuras...\n")
@@ -8,5 +8,11 @@ areas = []
 perimetros = []
 
 for index, row in dataFile.iterrows():
-    print(f"Fila {index}: Figura = {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['MEDIDA2']}")
+	figura = row['FIGURA']
+	m1 = row['MEDIDA1']
+	m2 = row['MEDIDA2']
+	a = area(figura, m1, m2)
+	areas.append(a)
+	print(f"Fila {index}: Figura = {row['FIGURA']}, Medida1 = {row['MEDIDA1']}, Medida2 = {row['MEDIDA2']}, Area = {a}")
+
 
